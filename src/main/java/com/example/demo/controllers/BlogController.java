432f761db7b93fcd13e0638007a10d800eb42d9a
NikolayLyapin <l.nick84@mail.ru>
2024-12-20 22:06:52 +0400

    Commit Test

--- src/main/java/com/example/demo/controllers/BlogController.java
+++ src/main/java/com/example/demo/controllers/BlogController.java
@@ -46,6 +46,9 @@ public class BlogController {
 
 
 
+
+
+
     @GetMapping("/blog/{id}/edit")
     public String blogEdit(@PathVariable(value = "id") long id, Model model){
         if(!postRepository.existsById(id)){
